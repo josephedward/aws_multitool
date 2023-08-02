77d290d2ee22e750923e6847f1d8bd41a38a3aff
josephedward <15126922+josephedward@users.noreply.github.com>
2023-08-02 17:21:31 -0400

    core

---
+++ core/browser.go
@@ -0,0 +1,118 @@
+package core
+
+import (
+	"os"
+	"os/exec"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/utils"
+	"github.com/ysmood/leakless"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+)
+
+func CustomLaunch() *rod.Browser{
+	// get the browser executable path
+	path := launcher.NewBrowser().MustGet()
+
+	// use the FormatArgs to construct args, this line is optional, you can construct the args manually
+	args := launcher.New().FormatArgs()
+
+	var cmd *exec.Cmd
+	if true { // decide whether to use leakless or not
+		cmd = leakless.New().Command(path, args...)
+	} else {
+		cmd = exec.Command(path, args...)
+	}
+
+	parser := launcher.NewURLParser()
+	cmd.Stderr = parser
+	utils.E(cmd.Start())
+	u := launcher.MustResolveURL(<-parser.URL)
+
+	return rod.New().ControlURL(u).MustConnect()
+}
+
+
+func UseSystemBrowser() *rod.Browser {
+	if path, exists := launcher.LookPath(); exists {
+		u := launcher.New().Bin(path).MustLaunch()
+		return rod.New().ControlURL(u).MustConnect()
+	}
+	return nil
+}
+
+
+func BrowserCliOutput() *rod.Browser{
+	// Pipe the browser stderr and stdout to os.Stdout .
+	u := launcher.New().Logger(os.Stdout).MustLaunch()
+	return rod.New().ControlURL(u).MustConnect()
+}
+
+func Manual(u string) *rod.Browser{
+	return rod.New().ControlURL(u).MustConnect()
+}
+
+var addr = flag.String("address", "localhost:7317", "the address to listen to")
+var quiet = flag.Bool("quiet", false, "silence the log")
+var allowAllPath = flag.Bool("allow-all", false, "allow all path set by the client")
+
+// A server to help launch browser remotely
+func Manager() {
+	flag.Parse()
+
+	m := launcher.NewManager()
+
+	if !*quiet {
+		m.Logger = log.New(os.Stdout, "", 0)
+	}
+
+	if *allowAllPath {
+		m.BeforeLaunch = func(l *launcher.Launcher, rw http.ResponseWriter, r *http.Request) {}
+	}
+
+	listen, err := net.Listen("tcp", *addr)
+	if err != nil {
+		utils.E(err)
+	}
+
+	if !*quiet {
+		fmt.Println("rod-manager listening on:", listen.Addr().String())
+	}
+
+	srv := &http.Server{Handler: m}
+	utils.E(srv.Serve(listen))
+
+}
+
+func Remote() {
+	// This example is to launch a browser remotely, not connect to a running browser remotely,
+	// to connect to a running browser check the "../connect-browser" example.
+	// Rod provides a docker image for beginers, run the below to start a launcher.Manager:
+	//
+	//     docker run -p 7317:7317 ghcr.io/go-rod/rod
+	//
+	// For more information, check the doc of launcher.Manager
+	l := launcher.MustNewManaged("")
+
+	// You can also set any flag remotely before you launch the remote browser.
+	// Available flags: https://peter.sh/experiments/chromium-command-line-switches
+	l.Set("disable-gpu").Delete("disable-gpu")
+
+	// Launch with headful mode
+	l.Headless(false).XVFB("--server-num=5", "--server-args=-screen 0 1600x900x16")
+
+	browser := rod.New().Client(l.MustClient()).MustConnect()
+
+	// You may want to start a server to watch the screenshots of the remote browser.
+	launcher.Open(browser.ServeMonitor(""))
+
+	fmt.Println(
+		browser.MustPage("https://mdn.dev/").MustEval("() => document.title"),
+	)
+
+	utils.Pause()
+}
---
+++ core/connection.go
@@ -0,0 +1,68 @@
+package core
+
+import (
+	"errors"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+)
+
+// for holding information about a login
+type WebsiteLogin struct {
+	Url      string
+	Username string
+	Password string
+}
+
+// for holding information about a rod connection
+type Connection struct {
+	Browser *rod.Browser
+	Page    *rod.Page
+}
+
+// should be able to work with most websites that use a login form
+func SimpleLogin(connect Connection, login WebsiteLogin) (Connection, error) {
+	//navigate to login page
+	page := connect.Page.MustNavigate(login.Url)
+
+	//Race Condition: It will keep polling until one selector has found a match
+	page.Race().Element("input[name='email']").MustHandle(func(e *rod.Element) {
+		e.MustInput(login.Username).MustType(input.Enter)
+	}).Element("input[name='username']").MustHandle(func(e *rod.Element) {
+		e.MustInput(login.Username).MustType(input.Enter)
+	}).MustDo()
+
+	page.MustElement("input[name='password']").MustInput(login.Password).MustType(input.Enter)
+
+	return Connection{Browser: connect.Browser, Page: page}, nil
+}
+
+func Connect(browser *rod.Browser, url string) (Connection) {
+	page := browser.MustPage(url)
+	return Connection{Browser: browser, Page: page}
+}
+
+
+func Login(login WebsiteLogin) (Connection, error) {
+	// Launch a new browser with default options, and connect to it.
+	browser := rod.New().MustConnect()
+
+	// Create a new page
+	page := browser.MustPage(login.Url)
+
+	//if browser is nil, page is nil
+	if browser == nil || page == nil {
+		return Connection{}, errors.New("browser or page is nil")
+	}
+
+	//login to the page
+	page.Race().Element("input[name='email']").MustHandle(func(e *rod.Element) {
+		e.MustInput(login.Username).MustType(input.Enter)
+	}).Element("input[name='username']").MustHandle(func(e *rod.Element) {
+		e.MustInput(login.Username).MustType(input.Enter)
+	}).MustDo()
+
+	page.MustElement("input[name='password']").MustInput(login.Password).MustType(input.Enter)
+
+	//create connection object to return
+	return Connection{Browser: browser, Page: page}, nil
+}
